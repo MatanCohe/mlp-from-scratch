32b5cef116d39242c5cdff8a35c40d3a7281daa9
Mor peled <morpeled5@gmail.com>
2019-05-07 14:47:20 +0300

    pytorch model improved : added validation test, train set normalization, dropout, model saving & graphs.

--- pytorch/model.py
+++ pytorch/model.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-from utils import TRAIN
+from utils import TRAIN, DEV
 import numpy as np
+import matplotlib.pyplot as plt
 
 NUMBER_OF_LABELS = 10
-
+folder_name = '../figures'
 # class Classifier(nn.Module):
 #     def __init__(self):
 #         super(Classifier, self).__init__()
@@ -25,10 +26,32 @@ NUMBER_OF_LABELS = 10
 #         correct_per_epoch = 0
 #         num_of_training_exm = train_loader.dataset.__len__()
 
-def train():
+
+def test(model):
+    x = DEV
+    # forward
+    y_hat = model(x[:, 1:])
+    y = (x[:, 0] - 1).to(torch.long)  # fix the classes since torch expects classes 0-9
+    # y_one_hot = np.zeros((y.shape[0], NUMBER_OF_LABELS))
+    # y_one_hot[np.arange(y.shape[0]), y.to(torch.int64)] = 1
+
+    loss = loss_function(y_hat, y)
+    average_loss = np.divide(loss.item(), DEV.shape[1])
+    pred = y_hat.argmax(dim=1)
+    accuracy = np.divide((y == pred).sum().data, DEV.shape[1])
+    return loss, accuracy
+
+def train(model):
     batch_size = 32
-    for k in range(number_of_epochs):
-        for i in range(0, TRAIN.shape[0], batch_size):
+    epochs_accuracy = []
+    epochs_loss = []
+    devs_loss = []
+    devs_accuracy = []
+    num_of_training_examples = TRAIN.shape[0]
+    for k in range(num_of_epochs):
+        epoch_correct_predictions = 0
+        epoch_loss = 0
+        for i in range(0, num_of_training_examples, batch_size):
             x = TRAIN[i:i + batch_size, :]  # x contains a batch
             # forward
             y_hat = model(x[:, 1:])
@@ -36,33 +59,55 @@ def train():
             # y_one_hot = np.zeros((y.shape[0], NUMBER_OF_LABELS))
             # y_one_hot[np.arange(y.shape[0]), y.to(torch.int64)] = 1
 
-            loss = loss_function(y_hat, y)
+            batch_loss = loss_function(y_hat, y)
+            epoch_loss += batch_loss.item()
             pred = y_hat.argmax(dim=1)
-            batch_accuracy = np.divide((y == pred).sum().data, float(y.shape[0])) * 100
-            print("epoch", k, ",batch", np.divide(i, batch_size), ",loss is:", loss.item(), ",batch accuracy is:", batch_accuracy.item(), "%")
+            batch_correct_predictions = (y == pred).sum().data
+            batch_accuracy = np.divide(batch_correct_predictions, float(y.shape[0])) * 100
+            epoch_correct_predictions += batch_correct_predictions
+            print("epoch", k, ",batch", np.divide(i, batch_size), ",loss is:", batch_loss.item(), ",batch accuracy is:", batch_accuracy.item(), "%")
+
             model.zero_grad()
-            loss.backward()
+            batch_loss.backward()
 
             # weight update (using gradient descent)
             with torch.no_grad():
                 for param in model.parameters():
                     param -= learning_rate * param.grad
+        epochs_accuracy.append(np.divide(epoch_correct_predictions, num_of_training_examples))
+        epochs_loss.append(np.divide(epoch_loss, num_of_training_examples))
+
+        # test on validation set
+        validation_loss, validation_accuracy = test(model)
+        devs_accuracy.append(validation_accuracy)
+        devs_loss.append(validation_loss)
 
 
-model = nn.Sequential(
+    # draw plots for train
+    plt.plot(np.arange(0, len(epochs_loss), 1), epochs_loss, 'r')
+    plt.savefig(folder_name + "/train_loss.png")
+    plt.clf()
+    plt.plot(np.arange(0, len(epochs_accuracy), 1), epochs_accuracy, 'r')
+    plt.savefig(folder_name + "/train_accuracy.png")
+    plt.clf()
+    # draw plots for validation
+    plt.plot(np.arange(0, len(devs_loss), 1), epochs_loss, 'r')
+    plt.savefig(folder_name + "/validation_loss.png")
+    plt.clf()
+    plt.plot(np.arange(0, len(devs_accuracy), 1), epochs_accuracy, 'r')
+    plt.savefig(folder_name + "/validation_accuracy.png")
+    plt.clf()
+
+my_model = nn.Sequential(
     torch.nn.Linear(3072, 1024),
     torch.nn.ReLU(),
-    torch.nn.Linear(1024, 1024),
-    torch.nn.ReLU(),
-    torch.nn.Linear(1024, 512),
-    torch.nn.ReLU(),
-    torch.nn.Linear(512, 10),
-
+    torch.nn.Dropout(0.3),
+    torch.nn.Linear(1024, 10)
 )
 
 loss_function = torch.nn.CrossEntropyLoss(reduction='sum')
 learning_rate = 0.0001
-number_of_epochs = 50
+num_of_epochs = 40
 
-train()
-
+train(my_model)
+torch.save(my_model.state_dict(), 'model_file')
--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1,9 +1,18 @@
 import torch
 import pandas as pd
+import numpy as np
 
 
 DEV = pd.read_csv('../data/validate.csv', header=None)
 DEV = torch.from_numpy(DEV.values).to(torch.float)
+
 TRAIN = pd.read_csv('../data/train.csv', header=None)
+# normalize train data
+mean = TRAIN.values[:, 1:].mean()
+std = TRAIN.values[:, 1:].std()
+# choose the columns indexes we wish to change
+idx = np.arange(1, TRAIN.shape[1])
+TRAIN[idx] = np.divide(TRAIN[idx] - mean, std)
+
 TRAIN = torch.from_numpy(TRAIN.values).to(torch.float)
 
