381724c90a45665ddc26021d36e4c89010cdbb61
Matan Cohen <matancohe@gmail.com>
2019-08-03 14:12:34 +0300

    Added batchnorm and fixed few things in Conv2 layer

--- layers.py
+++ layers.py
@@ -110,12 +110,86 @@ class Conv2d:
     
     def forward(self, previous_layer_output, is_training=False):
         self.prev_a = previous_layer_output
-        return convolution.conv4d(previous_layer_output, self.kernels)
+        z = convolution.conv4d(previous_layer_output, self.kernels)
+        a = self.activation(z)
+        self.z, self.a = z, a
+        return a
     
     def backward(self, da):
+        da = da * self.activation_derivative(self.z)
         new_da, self.dw, self.db = backword.backward_conv5d(self.prev_a, da, self.kernels)
         return new_da
     
     def weights_update(self, learning_rate, l2_lambda, batch_size):
+        dw, db = self.dw, self.db
         self.kernels = self.kernels - learning_rate * self.dw
         self.b = self.b - learning_rate * self.db
+        
+        
+class BatchNorm2d:
+    
+    def __init__(self, gamma, beta, activation_function, activation_function_derivative):
+        self.gamma, self.beta = gamma, beta
+        self.activation = activation_function
+        self.activation_derivative = activation_function_derivative
+    
+    def forward(self, previous_layer_output, is_training=False):
+        self.prev_a = previous_layer_output
+        N, C, H, W = previous_layer_output.shape
+        reshaped = previous_layer_output.reshape(N, -1)
+        X = self._batchnorm_forward(reshaped)
+        z = X.reshape(N, C, H, W)
+        a = self.activation(z)
+        self.z, self.a = z, a
+        return a
+        
+    
+    def backward(self, da):
+        N, C, H, W = da.shape
+        da = da * self.activation_derivative(self.z)
+        da_reshaped = da.reshape(N, -1)
+        dX = self._batchnorm_backword(da_reshaped)
+        dx_reshaped = dX.reshape(N, C, H, W)
+        return dx_reshaped
+    
+    def weights_update(self, learning_rate, l2_lambda, batch_size):
+        gamma, beta = self.gamma, self.beta
+        dgamma, dbeta = self.dgamma, self.dbeta
+        alpha = learning_rate
+        new_gamma = gamma - dgamma * alpha
+        new_beta = beta - dbeta * alpha
+        
+        self.gamma, self.beta = new_gamma, new_beta
+    
+    
+    def _batchnorm_forward(self, X):
+        gamma, beta = self.gamma, self.beta
+        mu = np.mean(X, axis=0)
+        var = np.var(X, axis=0)
+
+        X_norm = (X - mu) / np.sqrt(var + 1e-8)
+        out = gamma * X_norm + beta
+        self.X, self.X_norm, self.mu, self.var = X, X_norm, mu, var
+
+        return out
+    
+    def _batchnorm_backword(self, da):
+        X, X_norm, mu, var, gamma, beta = self.X, self.X_norm, self.mu, self.var, self.gamma, self.beta
+
+        N, D = X.shape
+
+        X_mu = X - mu
+        std_inv = 1. / np.sqrt(var + 1e-8)
+
+        dX_norm = da * gamma
+        dvar = np.sum(dX_norm * X_mu, axis=0) * -.5 * std_inv**3
+        dmu = np.sum(dX_norm * -std_inv, axis=0) + dvar * np.mean(-2. * X_mu, axis=0)
+
+        dX = (dX_norm * std_inv) + (dvar * 2 * X_mu / N) + (dmu / N)
+        dgamma = np.sum(da * X_norm, axis=0)
+        dbeta = np.sum(da, axis=0)
+        
+        self.dgamma, self.dbeta = dgamma, dbeta
+
+        return dX
+    
